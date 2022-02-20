a39b4d935e0d13d55f5bf49cd879c5f1b93ecf7e
omaals19 <omar.alsaudi.sy@gmail.com>
2022-02-20 13:53:01 +0100

    anything you want

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React, { FC } from 'react'
 import {Explanation} from './components/explanation/Explanation.tsx'
 import {HashRouter,Route} from 'react-router-dom'
-const MainPage: FC = () => {
-    return (
+import * as ReactDOM from 'react-dom';
+ReactDOM.render(
+    <React.StrictMode>
         <HashRouter>
-        <div
-            style={{
-                position: 'relative',
-                height: '100%',
-                display: 'grid',
-                gridTemplateRows: '900px auto'
-            }}
-        >
-          <Route path="/" exact component={Explanation} />
-            Hello
-        </div>
+            <div
+                style={{
+                    position: 'relative',
+                    height: '100%',
+                    display: 'grid',
+                    gridTemplateRows: '900px auto'
+                }}
+            >
+                <Route path="/" exact component={Explanation} />
+                Hello
+            </div>
         </HashRouter>
-    )
-}
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
-export default MainPage
