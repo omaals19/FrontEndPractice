4dcee5fd3482c7713028884845ca6023c9d0effd
Denis Markitanov <d.markitanov@vlife.kz>
2022-02-20 14:20:39 +0100

    feat(feat/denis): success

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { Explanation } from "./components/Explanation";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import * as ReactDOM from "react-dom";
+
+console.log("here");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <div
+        style={{
+          position: "relative",
+          height: "100%",
+          display: "grid",
+          gridTemplateRows: "900px auto",
+        }}
+      >
+        <Routes>
+          <Route path="/" exact element={<Explanation />} />
+        </Routes>
+      </div>
+    </HashRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
