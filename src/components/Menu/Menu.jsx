832b1412336dc5f97aac1b8aeb48afc388417d35
Denis Markitanov <d.markitanov@vlife.kz>
2022-03-05 16:10:45 +0100

    fix(feat/denis_menu): fix the menu stuff and new BrowserRouter

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,50 +1,52 @@
 import * as React from "react";
-import {useState} from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import {Link} from 'react-router-dom';
-import {SidebarData} from './SidebarData';
-import './Menu.css';
-import  {IconContext} from 'react-icons';
-import Logo from './whiteLogo.svg';
-function Menu () {
-    const [sidebar, setSidebar] = useState(false)
-    const showSidebar = () => setSidebar(!sidebar)
+import { useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { SidebarData } from "./SidebarData";
+import "./Menu.css";
+import { IconContext } from "react-icons";
+import Logo from "./whiteLogo.svg";
+function Menu() {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
 
-
-    return (
-        <>
-         <IconContext.Provider value={{color: '#fff'}}>
-
+  return (
+    <div>
+      <IconContext.Provider value={{ color: "#fff" }}>
         <div className="Menu">
-        <Link to="#" className='menu-bars'>
-            <FaIcons.FaBars onClick ={showSidebar}/>
-        </Link>
-            <img className='logo' alt='daily smarty ui image logo big' src={Logo}/>
+          <Link to="#" className="menu-bars">
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+          <img
+            className="logo"
+            alt="daily smarty ui image logo big"
+            src={Logo}
+          />
         </div>
 
-        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'} >
-            <ul className='nav-menu-items' onClick={showSidebar}>
-                <li className='navbar-toggle'>
-                    <Link to='#' className = 'menu-bars'>
-                        <AiIcons.AiOutlineClose />
-                    </Link>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <Link to="#" className="menu-bars">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
                 </li>
-                {SidebarData.map((item, index) => {
-                    return (
-                        <li key={index} className={item.cName}>
-                            <Link to={item.path}>
-                                {item.icon}
-                                <span>{item.title}</span>
-                            </Link>
-                        </li>
-                    );
-                })}
-            </ul>
+              );
+            })}
+          </ul>
         </nav>
-         </IconContext.Provider>
-        </>
-    )
+      </IconContext.Provider>
+    </div>
+  );
 }
 
 export default Menu;
--- src/index.js
+++ src/index.js
@@ -2,43 +2,43 @@ import React from "react";
 
 import { Trying } from "./components/Trying";
 import { Explanation } from "./components/Explanation";
-import {Omar, Stream} from "./Pages";
-import { Gps } from "./Pages"
+import { Omar, Stream } from "./Pages";
+import { Gps } from "./Pages";
 import { Menu } from "./components/Menu";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import * as ReactDOM from "react-dom";
-import {Provider} from 'react-redux';
+import { Provider } from "react-redux";
 import "./index.css";
-import {createStore} from "redux";
-import {counterReducer} from "./Reducers";
+import { createStore } from "redux";
+import { counterReducer } from "./Reducers";
 import store2 from "./Reducers/counterReducer";
+
 console.log("here");
+
 export const store = createStore(counterReducer);
 ReactDOM.render(
-
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={store}>
-      <div
-        style={{
-          position: "relative",
-          height: "100%",
-          display: "grid",
-          gridTemplateRows: "900px auto",
-        }}
-      >
+        <Menu />
+        <div
+          style={{
+            height: "100%",
+            display: "grid",
+            gridTemplateRows: "900px auto",
+          }}
+        >
           <Routes>
-            <Route path="/" exact element={<Menu />} /> // pass page components
+            <Route path="/" exact element={<div />} /> // pass page components
             // in element prop, i substituded it with div
-            <Route path="/About_us" element={<div  />} />
-            <Route path="/Stream"  element={<Stream />} />
+            <Route path="/About_us" element={<div />} />
+            <Route path="/Stream" element={<Stream />} />
             <Route path="/Gps" element={<Gps />} />
-            <Route path="/Omar" element={<Omar /> } />
+            <Route path="/Omar" element={<Omar />} />
           </Routes>
-      </div>
-    </Provider>
-    </HashRouter>
-  </React.StrictMode>
-    ,
+        </div>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById("root")
 );
